45d9d324227ad2af08f0a60c74ff26c52cb41224
adilosch <adilosch@hotmail.com>
2015-09-10 18:30:16 +0200

    add new test -> as feature branch

--- src/test/java/demo/Class1Test.java
+++ src/test/java/demo/Class1Test.java
@@ -21,5 +21,14 @@ public class Class1Test {
 		
 		assertFalse(class1.add(2, 2) == 5);
 	}
+	
+	@Test
+	public void test2() {
+
+		Class1 class1 = new Class1();
+		
+		assertFalse(class1.add(3, 2) == 5);
+	}
+
 
 }
