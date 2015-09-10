13f19a52dd6c8f585f879333cdbefc3564d5b747
adilosch <adilosch@hotmail.com>
2015-09-10 18:08:52 +0200

    expand test

--- src/test/java/demo/Class1Test.java
+++ src/test/java/demo/Class1Test.java
@@ -7,11 +7,19 @@ import org.junit.Test;
 public class Class1Test {
 
 	@Test
-	public void test() {
+	public void testPositive() {
 
 		Class1 class1 = new Class1();
 		
 		assertTrue(class1.add(2, 2) == 4);
+	}
+	
+	@Test
+	public void testNegative() {
+
+		Class1 class1 = new Class1();
+		
+		assertFalse(class1.add(2, 2) == 5);
 	}
 
 }
